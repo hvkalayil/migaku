b405adf7d12bf09fdc6714fa7cfe8c3499114960
hvkalayil <hoseakalayil@gmail.com>
2025-03-18 11:48:22 +0530

    added porfolio

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # migaku
 Named after the Japanese word "migaku" (磨く), meaning "to grind" or "to polish," reflecting the process of refining and perfecting through persistent effort. This name was chosen to embody the relentless optimization and care put into crafting my projects. Migaku operates as a central hub for running my projects
+
+## Working
+Run `sync.sh` to sync the projects
+Run `docker compose up` to start the projects
+Once the projects are up the cloudflare tunnel will serve the apps to the subdomains as mapped
---
+++ sync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CSV_FILE="projects.csv"
+
+# Create 'projects' folder if it doesn't exist
+mkdir -p projects
+
+tail -n +2 "$CSV_FILE" | while IFS=',' read -r _ GIT_URL WEB_URL LOCAL_URL; do
+    echo -e "\e[1m\e[42m Cloning/Updating $(basename "$GIT_URL" .git) \e[0m"
+    echo -e "\t\e[1mWeb: \e[0m$WEB_URL"
+    echo -e "\t\e[1mLocal: \e[0m$LOCAL_URL"
+
+    REPO_NAME=$(basename "$GIT_URL" .git)
+    if [ -d "projects/$REPO_NAME/.git" ]; then
+        echo -e "\t\e[1;34mUpdating existing repository...\e[0m"
+        (cd "projects/$REPO_NAME" && git pull --quiet)
+    else
+        echo -e "\t\e[1;32mCloning new repository...\e[0m"
+        (cd projects && git clone --quiet "$GIT_URL")
+    fi
+
+done
